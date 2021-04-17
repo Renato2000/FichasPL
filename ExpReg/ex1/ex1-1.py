360f60225bc55a5a922d92eda74f851cbf8cbe77
Renato <renatoandre2000@hotmail.com>
2021-04-17 16:29:30 +0100

    Correções

--- ExpReg/ex1/ex1-1.py
+++
@@ -1,9 +0,0 @@
-import re
-
-f = open("file.txt", "r")
-
-for linha in f:
-    (new_text, n) = re.subn(r'^(?i:ELE:)', "" , linha)
-    if n == 0:
-        new_text = re.sub(r'^(?i:EU:)', "", linha)
-    print(new_text)
